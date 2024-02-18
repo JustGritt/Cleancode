6078c7a2d40304edb315d78119684451e3b5705f
JustGritt <alexistan9712@gmail.com>
2024-02-18 17:30:00 +0100

    Testing card categories update

--- back/app/tests/card.test.js
+++ back/app/tests/card.test.js
@@ -1,3 +1,13 @@
+const category = {
+    FIRST: "FIRST",
+    SECOND: "SECOND",
+    THIRD: "THIRD",
+    FOURTH: "FOURTH",
+    FIFTH: "FIFTH",
+    SIXTH: "SIXTH",
+    SEVENTH: "SEVENTH"
+}
+
 class Card {
     constructor(id, category, question, answer, tag) {
         this.id = id;
@@ -5,6 +15,21 @@ class Card {
         this.question = question;
         this.answer = answer;
         this.tag = tag;
+    }
+
+    // Create a function to check if the user's answer is correct, and if so, update the card's category
+    checkAnswer(userAnswer) {
+        const isAnswerCorrect = userAnswer === this.answer;
+
+        if (isAnswerCorrect) {
+            // Get the categories
+            const categories = Object.keys(category);
+            const currentIndex = categories.indexOf(this.category);
+
+            this.category = currentIndex < categories.length - 1 ? categories[currentIndex + 1] : undefined;
+        } else {
+            this.category = category.FIRST;
+        }
     }
 }
 
@@ -25,10 +50,28 @@ describe('Card Service', () => {
         expect(card.question).toBe("What is the first question?");
     });
 
-    it.todo('Should update the card category when the user successfully answers the question');
+    it('Should update the card category when the user successfully answers the question', async () => {
+        const card = new Card("1", "FIRST", "What is the first question?", "This is the first answer", "Testing Tag");
+        const userAnwser = "This is the first answer";
 
-    it.todo('Should update the card category to FIRST when the user fails to answer the question');
+        card.checkAnswer(userAnwser);
+        expect(card.category).toBe("SECOND");
+    });
 
-    it.todo('Should remove the card from the database when the category hits SEVENTH');
+    it('Should update the card category to FIRST when the user fails to answer the question', async () => {
+        const card = new Card("1", "FIRST", "What is the first question?", "This is the first answer", "Testing Tag");
+        const userAnwser = "This is not the first answer";
+
+        card.checkAnswer(userAnwser);
+        expect(card.category).toBe("FIRST");
+    });
+
+    it('Should remove the card from the database when the category hits SEVENTH', async () => {
+        const card = new Card("1", "SEVENTH", "What is the seventh question?", "This is the seventh answer", "Testing Tag");
+        const userAnwser = "This is the seventh answer";
+
+        card.checkAnswer(userAnwser);
+        expect(card.category).toBe(undefined);
+    });
 
 })
