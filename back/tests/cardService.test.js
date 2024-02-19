b831259bdad3eb5d9640fc65b7cd1555314265c3
Charles Parames <55605195+popokola@users.noreply.github.com>
2024-02-19 21:14:05 +0000

    integration test cardServive 401 when token not set and refacto cardService

--- back/tests/cardService.test.js
+++ back/tests/cardService.test.js
@@ -2,22 +2,29 @@ const { createServer  } = require('./testGlobalSetup');
 const cardsService  = require('../services/card');
 
 const server = createServer();
+let Token = '';
 
 describe('Cards API', () => {  
+    beforeAll(async () => {
+        await server.post('/register').send({
+            "email": "test@example.com",
+            "password": "password",
+        });
+        const data = await server.post('/login').send({
+            "email": "test@example.com",
+            "password": "password",
+        });
+        Token = data.body.token;
+    });
     describe('GET /cards', () => {
         it('should retrieve all cards by array of tags', async () => {
-           //create a new user
-            const newUser = await server.post('/register').send({
-                "email": "test@example.com",
-                "password": "password",
-            });
-            const data = await server.post('/login').send({
-                "email": "test@example.com",
-                "password": "password",
-            });
-            const response = await server.get('/cards?tags=tag1,tag2').set('Authorization', `Bearer ${data.body.token}`);
+            const response = await server.get('/cards?tags=tag1,tag2').set('Authorization', `Bearer ${Token}`);
             expect(response.status).toBe(200);
             expect(response.body).toEqual(expect.any(Array));
+        });
+        it('should return 401 if no token is provided', async () => {
+            const response = await server.get('/cards?tags=tag1,tag2');
+            expect(response.status).toBe(401);
         });
     });
 });
